7ef64978f587a7e4392e4056eb7fff8ccc7d06e5
hinata333 <112308384+hinata333@users.noreply.github.com>
2022-09-08 14:51:58 +0900

    Update townpage.py

--- townpage.py
+++ townpage.py
@@ -4,11 +4,11 @@ from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 
 st.write('test')
-os.system('sbase install geckodriver')
-os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
-opts = FirefoxOptions()
-opts.add_argument("--headless")
-browser = webdriver.Firefox(options=opts)
+#os.system('sbase install geckodriver')
+#os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+#opts = FirefoxOptions()
+#opts.add_argument("--headless")
+#browser = webdriver.Firefox(options=opts)
 
-browser.get('http://example.com')
-st.write(browser.page_source)
+#browser.get('http://example.com')
+#st.write(browser.page_source)
