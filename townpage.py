11702127620c81c6e4249ca2af256f61ebf603ef
hinata333 <112308384+hinata333@users.noreply.github.com>
2022-09-08 14:51:30 +0900

    Update townpage.py

--- townpage.py
+++ townpage.py
@@ -3,6 +3,7 @@ import os, sys
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 
+st.write('test')
 os.system('sbase install geckodriver')
 os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
 opts = FirefoxOptions()
